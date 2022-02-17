85fa67dc7589df02f79014ae096f058c12dd2273
miuser <miuser@email.com>
2022-02-17 10:38:14 +0800

    init

---
+++ app/src/main/java/com/github/makewheels/battery/MainActivity.java
@@ -0,0 +1,35 @@
+package com.github.makewheels.battery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tv_info;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv_info = findViewById(R.id.tv_info);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = registerReceiver(null, ifilter);
+
+        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+        int batteryPct = (int) (level * 100 / (float) scale);
+        tv_info.setText(batteryPct + "");
+    }
+}
